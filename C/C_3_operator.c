83f0c18cfc4c5306f5b8d24c8ce3b73ade0946f9
loveiscomplicated <yyjsjsjs243@gmail.com>
2025-02-25 03:43:15 +0900

    25.02.24 update

---
+++ C/C_3_operator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    // arithmatic operation
+    int a = 10 % 3;
+    printf("%d \n", a);
+
+    a++;
+    printf("%d \n", a);
+
+    a--;
+    printf("%d \n", a);
+    /*
+    + - 더하기
+    - - 빼기
+    * - 곱하기
+    / - divide, if int, no 소수점
+    % - moduler
+    ++ - 1 더하기 (즉 a = a + 1)
+    -- - 1 빼기
+    */
+
+   // relational operation (5:13부터 보면 됨)
+    return 0;
+}
